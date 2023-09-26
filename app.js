0f90b526fdaede847873615c8766f0944d1f6fd3
casey <caseyochieng46@gmail.com>
2023-09-26 13:22:02 +0300

    fix the problem

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('Hello')
+const app = Vue.createApp({
+    data() {
+        return {
+            title: 'Vue Js Test',
+            author: 'John Doe',
+            age: 30
+        }
+    },
+})
+
+app.mount('#app')
+
+// This code snippet is written in Vue.js. It creates a new Vue application using the createApp method and assigns it to the app constant. The mount method is then called on the app instance to mount the Vue application to the element with the id app in the HTML document.
+
+
+
+
+
+
+
